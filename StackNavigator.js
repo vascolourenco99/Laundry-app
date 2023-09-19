4d2462930306ebb2c50692788b27b8358bdf3f30
vascolourenco <vascocorreia99@gmail.com>
2023-09-19 17:15:54 +0100

    pick up screen done

--- StackNavigator.js
+++ StackNavigator.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import PickUpScreen from "./screens/PickUpScreen";
+import CartScreen from "./screens/CartScreen";
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
@@ -12,7 +13,8 @@ const StackNavigator = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
-        <Stack.Screen name="PickUp" component={PickUpScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="PickUp" component={PickUpScreen}/>
+        <Stack.Screen name="Cart" component={CartScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
